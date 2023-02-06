ea906f4e2d845e1f630740737aed93bb70be76f5
seikyo-cho-lvgs <seikyo.cho@leverages.jp>
2023-02-06 11:55:56 +0900

    add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
+import { Analytics } from '@vercel/analytics/react';
+
 function MyApp({ Component, pageProps }) {
   return (
-    <Component {...pageProps} />
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
   );
 }
 
